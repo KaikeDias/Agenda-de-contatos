645137c966147613355051eede13f3f9b7eefb90
Kaike Dias <kaikedias98@gmail.com>
2022-11-09 10:35:33 -0300

    Implementação da classe Connection

---
+++ lib/database/sqlite/connection.dart
@@ -0,0 +1,23 @@
+import 'package:flutter_application_1/database/sqlite/script.dart';
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+
+class Connection {
+  static late Database _db;
+
+  static Future<Database> get() async {
+    String path = join(await getDatabasesPath(), 'banco1');
+    _db = await openDatabase(
+      path,
+      version: 1,
+      onCreate: (db, version) {
+        db.execute(createTable);
+        db.execute(insert1);
+        db.execute(insert2);
+        db.execute(insert3);
+      },
+    );
+
+    return _db;
+  }
+}
